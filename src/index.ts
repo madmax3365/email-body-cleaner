1fa001f1ad06146f53fceee65d7ef348727c7942
Gago <gagmuradyan93@gmail.com>
2019-11-13 12:08:48 +0400

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,3 @@ export = {
     return parser.parse(body);
   },
 };
-
-`Understood! Thanks mate !
-
-------------------------------
-*From:* somename lastname <somemail@gmail.com>
-*Sent:* Tuesday, November 12, 2019 6:07 PM
-*To:* anothermail@outlook.com <anothermail@outlook.com>
-*Subject:* Fwd: Urgent thing
-
-
-
----------- Forwarded message ---------
-From: *anothername anotherlastname* <anotherdude@gmail.com>
-Date: Tue, Nov 12, 2019 at 1:10 PM
-Subject: Re: Another urgent thing
-To: <somemail@gmail.com>
-
-Burning deadline
-
-On Tue, Nov 12, 2019 at 11:51 anothername anotherlastname <anotherdude@gmail.com> wrote:`;
